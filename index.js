003c69aa46de3dd69415ae7c4c69c3fff8fbde23
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-04-01 10:18:46 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(response => response.json())
+        .then(object => {
+            const idElement = document.createElement("p");
+            idElement.textContent = `New ID: ${object.id}`;
+            document.body.appendChild(idElement);
+        })
+        .catch(error => {
+            const errorElement = document.createElement("p");
+            errorElement.textContent = `Error: ${error.message}`;
+            document.body.appendChild(errorElement);
+        });
+}
